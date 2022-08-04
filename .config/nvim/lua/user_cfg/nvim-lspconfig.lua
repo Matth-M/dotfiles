cfa4f91772b1745d5f7533f7c11245964bbe1b02
Matth-M <matthieu.matsima@gmail.com>
2022-08-04 13:16:12 +0200

    Call the lsp-installer setup function
    The call of lsp-installer setup function wasn't called before the setup
    of the servers. This call is necessary to start lsp servers with
    lsp-installer (check https://github.com/williamboman/nvim-lsp-installer/blob/main/doc/nvim-lsp-installer.txt)

--- .config/nvim/lua/user_cfg/nvim-lspconfig.lua
+++ .config/nvim/lua/user_cfg/nvim-lspconfig.lua
@@ -1,3 +1,11 @@
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -28,6 +36,8 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
+require('nvim-lsp-installer').setup{}
+
 require('lspconfig')['pyright'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
