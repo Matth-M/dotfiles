84fffcd8e60c78ea0e715b710de4930e7969b34f
Matth-M <matthieu.matsima@gmail.com>
2022-08-07 23:13:46 +0200

    Change lualine theme to tokyonight

--- .config/nvim/lua/user_cfg/lualine.lua
+++ .config/nvim/lua/user_cfg/lualine.lua
@@ -6,7 +6,7 @@ end
 local config = {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox',
+    theme = 'tokyonight',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {},
