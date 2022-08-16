46da9c34ed8e3f4fdb1a95422cd60e18b7c6acb1
Matth-M <matthieu.matsima@gmail.com>
2022-08-16 03:02:41 +0200

    Add support for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,3 +5,11 @@ killall -q polybar
 
 # Launch bar
 # polybar default 2>&1 | tee /tmp/polybar.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload default &
+  done
+else
+  polybar --reload default &
+fi
