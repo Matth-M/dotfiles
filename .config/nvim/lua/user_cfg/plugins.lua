1943e45ab6b384a5360f942948b67fc629590f94
Matth-M <matthieu.matsima@gmail.com>
2022-12-17 19:48:05 +0100

    nvim: Add autocommand calling packer on save in plugins.lua

--- .config/nvim/lua/user_cfg/plugins.lua
+++ .config/nvim/lua/user_cfg/plugins.lua
@@ -4,6 +4,15 @@ if not status_ok then
   return
 end
 
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
 return packer.startup(function(use) -- :PackerSync
     -- utility
     use "wbthomason/packer.nvim"
