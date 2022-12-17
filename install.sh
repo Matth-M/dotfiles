e4b724492e70d64f0a7d78f637dd2081879cc5c8
Matth-M <matthieu.matsima@gmail.com>
2022-12-17 19:24:27 +0100

    Update install script and list of files to exclude

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ fi
 # Packages
 sudo $packet_manager_install firefox vlc gcc g++ vim i3-gaps thunderbird
 redshift steam keepassxc polybar brightnessctl @virtualization rofi \
-feh neofetch blueman ranger ipython3 htop pnpm exa ripgrep
+feh neofetch blueman ranger ipython3 htop pnpm exa ripgrep fzf
 
 pnpm install -g vtop
 
