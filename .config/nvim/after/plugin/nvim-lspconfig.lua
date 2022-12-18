734808b79ebdf08682d88731e1f1ce73e5b9766b
Matth-M <matthieu.matsima@gmail.com>
2022-12-18 23:58:27 +0100

    nvim: Add emmet lsp server

--- .config/nvim/after/plugin/nvim-lspconfig.lua
+++ .config/nvim/after/plugin/nvim-lspconfig.lua
@@ -119,3 +119,17 @@ require('lspconfig')['ghdl_ls'].setup {
 	flags = lsp_flags,
 	capabilities = capabilities
 }
+
+require('lspconfig')['emmet_ls'].setup {
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+}
