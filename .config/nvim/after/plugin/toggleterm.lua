8f72964c8a0876ae270db19ecc76f5f8c56200d0
Matth-M <matthieu.matsima@gmail.com>
2023-03-17 13:10:19 +0100

    nvim: Add toggleterm.lua config file

---
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,16 @@
+require("toggleterm").setup({
+	open_mapping = "<C-Space>",
+	direction = "float",
+	size = 20,
+	float_opts = {
+		-- The border key is *almost* the same as 'nvim_open_win'
+		-- see :h nvim_open_win for details on borders however
+		-- the 'curved' border is a custom border type
+		-- not natively supported but implemented in this plugin.
+		border = "curved",
+		-- like `size`, width and height can be a number or function which is passed the current terminal
+		width = vim.o.columns * 0.8,
+		height = 50,
+		-- winblend = 3,
+	},
+})
