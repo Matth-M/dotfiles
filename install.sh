8ba3bd81730235fec40144ac99dcc67cdb002bb1
Matth-M <matthieu.matsima@gmail.com>
2023-08-31 20:58:23 +0200

    update

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo dnf install -y firefox vlc gcc g++ vim i3-gaps thunderbird \
 redshift steam keepassxc polybar brightnessctl @virtualization rofi \
 feh neofetch blueman ranger ipython3 htop pnpm exa ripgrep fzf zsh \
 make cmake acpi xsel xclip spectacle xrandr util-linux-user nodejs \
-xclip
+xclip feh rofi polybar
 
 # Change default shell to zsh
 chsh -s /bin/zsh $USER
