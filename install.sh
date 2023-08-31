0a3386d8a2f59185e4ffe2d003aeb23ead19ddfc
Matth-M <93771840+Matth-M@users.noreply.github.com>
2023-08-31 19:37:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,46 +1,20 @@
 #!/bin/sh
 
-DISTRO=$(lsb_release -is) #Install lsb_release before hand
-if [[ $DISTRO == "Fedora" ]] then
-    packet_manager_install='dnf install -y'
-    elif [[ $DISTRO == "Ubuntu" ]] then
-    packet_manager_install='apt-get install -y'
-fi
-
 # Packages
-sudo $packet_manager_install firefox vlc gcc g++ vim i3-gaps thunderbird
+sudo dnf install -y firefox vlc gcc g++ vim i3-gaps thunderbird \
 redshift steam keepassxc polybar brightnessctl @virtualization rofi \
-    feh neofetch blueman ranger ipython3 htop pnpm exa ripgrep fzf zsh \
-    make cmake acpi xsel xclip spectacle xrandr
-
-pnpm install -g vtop
+feh neofetch blueman ranger ipython3 htop pnpm exa ripgrep fzf zsh \
+make cmake acpi xsel xclip spectacle xrandr util-linux-user nodejs \
+xclip
 
 # Change default shell to zsh
 chsh -s /bin/zsh $USER
 
+# Install Zap
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
+# Install starship prompt
+curl -sS https://starship.rs/install.sh | sh
+
 # Install kitty terminal emulator
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-
-#### NEOVIM ####
-# Build dependencies
-$packet_manager_install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip patch gettext curl
-
-# Build
-git clone https://github.com/neovim/neovim ~/git/neovim
-if $? ; then
-    cd ~/git/neovim
-    make CMAKE_BUILD_TYPE=RelWithDebInfo
-    sudo make install
-fi
-
-#### RUST ######
-
-# Install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# Check that cargo is installed
-if ! cargo --version ; then
-
-    # Install onefetch
-    cargo install onefetch
-fi
