62967b49aaedee6ef44287ee49b63e116f62ea74
Matth-M <matthieu.matsima@gmail.com>
2024-03-07 14:16:17 +0100

    update

--- .config/polybar/sblt.sh
+++ .config/polybar/sblt.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-bluetoothctl info >> /tmp/sbltlog
+bluetoothctl info >>/tmp/sbltlog
 
-if [ "$?" == 0 ]
-then
-	status=$(bluetoothctl info | awk '$1 == "Connected:"{print $0}' |sed 's/Connected: //g' |  sed  's/^[ \t]*//' )
-	stta=$(echo $status | sed  's/^[ \t]*//')
+if [ "$?" == 0 ]; then
+	status=$(bluetoothctl info | awk '$1 == "Connected:"{print $0}' | sed 's/Connected: //g' | sed 's/^[ \t]*//')
+	stta=$(echo $status | sed 's/^[ \t]*//')
 	stt=$(echo $stta)
-	if [ "$stt" == "yes" ]
-	then
- 		echo " "
+	if [ "$stt" == "yes" ]; then
+		# echo ""
+		echo "blt"
 	fi
 else
-	echo " "
+	echo "speaker"
 fi
