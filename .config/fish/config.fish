7c6a6b20d5a218e1bec26f7d9afe394e910f0181
Matth-M <matthieu.matsima@gmail.com>
2025-01-07 09:48:57 +0100

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,33 +1,38 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	source $HOME/.config/shell/aliasrc
+	alias reload="source $HOME/.config/fish/config.fish"
+
+	function bind_bang
+		switch (commandline -t)[-1]
+			case "!"
+				commandline -t -- $history[1]
+				commandline -f repaint
+			case "*"
+				commandline -i !
+		end
+	end
+
+	function bind_dollar
+		switch (commandline -t)[-1]
+			case "!"
+				commandline -f backward-delete-char history-token-search-backward
+			case "*"
+				commandline -i '$'
+		end
+	end
+
+	function fish_user_key_bindings
+		bind ! bind_bang
+		bind '$' bind_dollar
+	end
+
+	set PATH ~/.local/bin ~/.local/bin/scripts $PATH
+	set fish_greeting
 end
 
-source $HOME/.config/shell/aliasrc
-alias reload="source $HOME/.config/fish/config.fish"
-
-function bind_bang
-    switch (commandline -t)[-1]
-        case "!"
-            commandline -t -- $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
-    end
+if status is-login
+	if test [ -z "$DISPLAY" ]; and test [ "$XDG_VTNR" -le 3 ]
+	  exec startx
+	end
+	export QSYS_ROOTDIR="/home/matth/.cache/paru/clone/quartus-free/pkg/quartus-free-quartus/opt/intelFPGA/23.1/quartus/sopc_builder/bin"
 end
-
-function bind_dollar
-    switch (commandline -t)[-1]
-        case "!"
-            commandline -f backward-delete-char history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
-end
-
-function fish_user_key_bindings
-    bind ! bind_bang
-    bind '$' bind_dollar
-end
-
-set PATH ~/.local/bin ~/.local/bin/scripts $PATH
-set fish_greeting
--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -2,16 +2,16 @@
 return {
 	{
 		"iamcco/markdown-preview.nvim",
-		enabled = false,
+		enabled = true,
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
-		config = function()
-			vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", { desc = "MarkdownPreview" })
-			vim.keymap.set("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", { desc = "MarkdownPreview" })
-			vim.keymap.set("n", "<leader>mt", "<cmd>MarkdownPreviewToggle<cr>", { desc = "MarkdownPreview" })
-		end,
 		build = function()
 			vim.fn["mkdp#util#install"]()
+		end,
+		config = function()
+			vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", { desc = "MarkdownPreview" })
+			vim.keymap.set("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", { desc = "MarkdownPreviewStop" })
+			vim.keymap.set("n", "<leader>mt", "<cmd>MarkdownPreviewToggle<cr>", { desc = "MarkdownPreviewToggle" })
 		end,
 	},
 	{
@@ -23,6 +23,6 @@ return {
 		opts = {},
 		-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 	},
 }
