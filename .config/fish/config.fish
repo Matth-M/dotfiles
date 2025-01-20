b2af558eb0aba45fbcbdfb3adce5ad15d3e272e5
Matth-M <matthieu.matsima@gmail.com>
2025-01-20 01:40:10 +0100

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,10 +29,10 @@ if status is-interactive
 	set PATH ~/.local/bin ~/.local/bin/scripts $PATH
 	set fish_greeting
 end
-#
-# if status is-login
-# 	if test [ -z "$DISPLAY" ]; and test [ "$XDG_VTNR" -le 3 ]
-# 	  exec startx
-# 	end
-# 	export QSYS_ROOTDIR="/home/matth/.cache/paru/clone/quartus-free/pkg/quartus-free-quartus/opt/intelFPGA/23.1/quartus/sopc_builder/bin"
-# end
+
+if status is-login
+	if test -z "$DISPLAY"; and test "$XDG_VTNR" -le 3
+	  exec startx
+	end
+	export QSYS_ROOTDIR="/home/matth/.cache/paru/clone/quartus-free/pkg/quartus-free-quartus/opt/intelFPGA/23.1/quartus/sopc_builder/bin"
+end
